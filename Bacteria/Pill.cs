3ff45bf581617a56473d9f146ae597049ea1375f
Patrycja <patcho2808@gmail.com>
2018-11-04 11:19:22 +0100

    Pill can't go outside window

--- Bacteria/Pill.cs
+++ Bacteria/Pill.cs
@@ -9,33 +9,42 @@ namespace Bacteria
     class Pill : Object
     {
 
-        private float speed = 0.1f; 
+        private float speed = 0.1f;
+        private SFML.System.Vector2f WindowSize; 
 
         public Pill(string path, SFML.System.Vector2f WindowSize ) : base(path)
         {
-            Sprite.Position = new SFML.System.Vector2f(WindowSize.X, WindowSize.Y);
+            Sprite.Position = new SFML.System.Vector2f(WindowSize.X/2, WindowSize.Y/2);
             Sprite.Scale = new SFML.System.Vector2f(2, 2);
             Sprite.Rotation = 90;
+
+            this.WindowSize = WindowSize; 
         }
 
         public void MoveRight()
         {
-            Sprite.Position = new SFML.System.Vector2f(Sprite.Position.X + speed, Sprite.Position.Y); 
+            if(Sprite.Position.X < WindowSize.X - Sprite.GetGlobalBounds().Width/2)
+                Sprite.Position = new SFML.System.Vector2f(Sprite.Position.X + speed, Sprite.Position.Y);
         }
 
         public void MoveLeft()
         {
-            Sprite.Position = new SFML.System.Vector2f(Sprite.Position.X - speed, Sprite.Position.Y);
+            if (Sprite.Position.X > Sprite.GetGlobalBounds().Width / 2)
+                Sprite.Position = new SFML.System.Vector2f(Sprite.Position.X - speed, Sprite.Position.Y);
+
         }
 
         public void MoveUp()
         {
-            Sprite.Position = new SFML.System.Vector2f(Sprite.Position.X, Sprite.Position.Y - speed);
+            if (Sprite.Position.Y > Sprite.GetGlobalBounds().Height / 2)
+                Sprite.Position = new SFML.System.Vector2f(Sprite.Position.X, Sprite.Position.Y - speed);
         }
 
         public void MoveDown()
         {
-            Sprite.Position = new SFML.System.Vector2f(Sprite.Position.X, Sprite.Position.Y + speed);
+            if (Sprite.Position.Y < WindowSize.Y - Sprite.GetGlobalBounds().Height / 2)
+                Sprite.Position = new SFML.System.Vector2f(Sprite.Position.X, Sprite.Position.Y + speed);
         }
+
     }
 }
