562beb450475410dbb4c670c4acb1aa6ab5af87f
Patrycja <patcho2808@gmail.com>
2018-11-12 10:36:14 +0100

    Added EndindText.cs

---
+++ Bacteria/EndindText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace Bacteria
+{
+    class EndindText : TextObject
+    {
+        public EndindText(Font newFont, SFML.System.Vector2f WindowSize) : base(newFont,WindowSize)
+        {
+
+        }
+    }
+}
